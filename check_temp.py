f6a856573ed99ba84f614cd2d170d4f670eab9a5
Manu Cabello <manu@manusoft.es>
2020-03-24 00:51:15 +0100

    Código terminado

--- check_temp.py
+++ check_temp.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import json
 import requests
+import RPi.GPIO as GPIO
 
 def cpu_temp():
 	thermal_zone = subprocess.Popen(['cat', '/sys/class/thermal/thermal_zone0/temp'], stdout=subprocess.PIPE)
@@ -30,9 +31,16 @@ def send_warning(message):
 def check_temp():
 	cpu = cpu_temp()
 	gpu = gpu_temp()
-	if float(cpu) > 50 or float(gpu) > 50:
+	if (float(cpu) > 45 or float(gpu) > 45) and not GPIO.input(7):
+		GPIO.output(7, True)
+		send_warning("CPU: "+str(cpu)+"º\nGPU: "+str(gpu)+"º")
+	elif float(cpu) <= 40 and float(gpu) <= 40 and GPIO.input(7):
+		GPIO.output(7, False)
 		send_warning("CPU: "+str(cpu)+"º\nGPU: "+str(gpu)+"º")
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
 check_temp()
 
 print ("CPU: "+str(cpu_temp())+"º")
